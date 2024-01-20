f85668012e8fc803c80d96f075dd5d70aa2b145f
MohammadKhalil2062002 <155315671+MohammadKhalil2062002@users.noreply.github.com>
2024-01-20 14:59:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+  
+function twoSum(nums, target) {
+  let map = new Map();
+   
+  for (let i = 0; i < nums.length; i++) {
+     let complement = target - nums[i];
+     
+     if (map.has(complement)) {
+       return [map.get(complement), i];
+     }
+     
+     map.set(nums[i], i);
+  }
+   
+  return [];
+ }  twoSum([2,7,11,15],9) 
